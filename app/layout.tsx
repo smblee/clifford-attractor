27b922126456c08464db9aede52a747a67a8f86b
Bryan Lee <smblee14@gmail.com>
2025-10-28 04:51:29 +0900

    yo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Chaos attractors',
+  description: 'Chaos attractors',
 }
 
 export default function RootLayout({
